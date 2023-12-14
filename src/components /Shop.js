6a6e57110cc100db1c4248bb30c5427f27bf052b
Brian Lawi Ochiel <lawioochiel@gmail.com>
2023-12-14 18:50:35 +0300

    changes to data in json file and optimisation

--- src/components /Shop.js
+++ src/components /Shop.js
@@ -3,51 +3,57 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useCart } from './CartContext';
 
+
 function Shop() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const { addToCart } = useCart();
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const { addToCart } = useCart();
 
-  useEffect(() => {
+    useEffect(() => {
     // Simulating fetching data from an API
-    fetch('http://localhost:3000/Stock')
-      .then(response => response.json())
-      .then(data => {
-        setData(data);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-        setError('An error occurred while fetching data.');
-        setLoading(false);
-      });
-  }, []);
+        fetch('http://localhost:3000/Stock')
+            .then(response => response.json())
+            .then(data => {
+                setData(data);
+                setLoading(false);
+        })
+            .catch(error => {
+            console.error('Error fetching data:', error);
+            setError('An error occurred while fetching data.');
+            setLoading(false);
+        });
+    }, []);
 
-  return (
-    <div className="shop-container">
-      {loading && <p className="loading-message">Loading...</p>}
-      {error && (
-        <div>
-          <p className="error-message">{error}</p>
-          <button onClick={() => window.location.reload()}>Retry</button>
-        </div>
-      )}
-      {data.map(item => (
+    return (
+        <div className="shop-container">
+        {loading && <p className="loading-message">Loading...</p>}
+        {error && (
+            <div>
+                
+                <p className="error-message">{error}</p>
+                <button onClick={() => window.location.reload()}>Retry</button>
+            </div>
+        )}
+            {/* <div className='shopTitle'>
+                <h2>DRINK OF CHOICE</h2>
+            </div> */}
+        {data.map(item => (
+            
         <div key={item.id} className="shop-item">
-          <img src={item.Image} alt={item.Name} className="item-image" />
-          <p className="item-name">Name: {item.Name}</p>
-          <p className="item-description">Description: {item.Description}</p>
-          <p className="item-price">Price: {item.Price} Ksh</p>
-          <button onClick={() => addToCart(item)}>Add to Cart</button>
+            <img src={item.Image} alt={item.Name} className="item-image" />
+            <p className="item-name">Name: {item.Name}</p>
+            <p className="item-description">Description: {item.Description}</p>
+            <p className="item-price">Price: {item.Price} Ksh</p>
+            <button onClick={() => addToCart(item)}>Add to Cart</button>
         </div>
-      ))}
-      {/* Floating Cart Icon */}
-      <div className="floating-cart-icon">
+    ))}
+        {/* Floating Cart Icon */}
+        <div className="floating-cart-icon">
         <Link to="/Cart">🛒</Link>
-      </div>
     </div>
-  );
+    </div>
+    );
 }
 
 export default Shop;
